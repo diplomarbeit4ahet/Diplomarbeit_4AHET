fb52ef6f95df2468db555b9b5617dbb1fd955e2c
Timo <timo.edlinger@bulme.at>
2017-07-03 18:52:21 +0200

    Initial commit

---
+++ src/main/java/Diplomarbeit/main.java
@@ -0,0 +1,10 @@
+package Diplomarbeit;
+
+/**
+ * Created by Timo on 03.07.2017.
+ */
+public class main {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
